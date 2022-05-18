d896a7e8ea8da945b808e5fbca2ab784a27800f7
Seednode <git@seedno.de>
2022-05-17 20:38:21 -0500

    Removed test counter from web output

--- web/web.go
+++ web/web.go
@@ -9,22 +9,17 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strconv"
 
 	cockroach "seedno.de/seednode/commands-web/cockroach"
 )
 
 func ServePage() {
-
-	var counter int = 1
-
 	h1 := func(w http.ResponseWriter, _ *http.Request) {
 		results, err := cockroach.RunQuery()
 		if err != nil {
 			panic(err)
 		}
-		io.WriteString(w, strconv.Itoa(counter)+"\n"+fmt.Sprintln(results))
-		counter += 1
+		io.WriteString(w, fmt.Sprintln(results))
 	}
 
 	http.HandleFunc("/", h1)
