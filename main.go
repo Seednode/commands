1ea16766a22a2c901df9e767adbf85d638537a3b
Seednode <github@seedno.de>
2025-03-06 13:38:14 -0500

    Add security headers, HTTPS support, move everything out of cmd/ package

--- main.go
+++ main.go
@@ -1,20 +1,126 @@
 /*
-Copyright © 2024 Seednode <seednode@seedno.de>
+Copyright © 2025 Seednode <seednode@seedno.de>
 */
 
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"strings"
 
-	cmd "seedno.de/seednode/commands-web/cmd"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const (
+	ReleaseVersion string = "1.0.0"
+)
+
+var (
+	databaseType     string
+	databaseHost     string
+	databasePort     string
+	databaseUser     string
+	databasePass     string
+	databaseName     string
+	databaseTable    string
+	databaseSslMode  string
+	databaseRootCert string
+	databaseSslCert  string
+	databaseSslKey   string
+	bind             string
+	port             uint16
+	profile          bool
+	scheme           string = "http"
+	tlsCert          string
+	tlsKey           string
+	verbose          bool
+	version          bool
 )
 
 func main() {
-	cmd := cmd.NewRootCommand()
+	cmd := &cobra.Command{
+		Use:   "commands",
+		Short: "Display command logs from a database.",
+		Args:  cobra.ExactArgs(0),
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			initializeConfig(cmd)
+
+			if tlsCert == "" && tlsKey != "" || tlsCert != "" && tlsKey == "" {
+				return errors.New("TLS certificate and keyfile must both be specified to enable HTTPS")
+			}
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return ServePage()
+		},
+	}
+
+	cmd.Flags().StringVar(&databaseType, "db-type", "", "database type to connect to")
+	cmd.Flags().StringVar(&databaseHost, "db-host", "", "database host to connect to")
+	cmd.Flags().StringVar(&databasePort, "db-port", "", "database port to connect to")
+	cmd.Flags().StringVar(&databaseUser, "db-user", "", "database user to connect as")
+	cmd.Flags().StringVar(&databasePass, "db-pass", "", "database password to connect with")
+	cmd.Flags().StringVar(&databaseName, "db-name", "", "database name to connect to")
+	cmd.Flags().StringVar(&databaseTable, "db-table", "", "database table to query")
+	cmd.Flags().StringVar(&databaseSslMode, "db-ssl-mode", "", "database ssl connection mode")
+	cmd.Flags().StringVar(&databaseRootCert, "db-root-cert", "", "database ssl root certificate path")
+	cmd.Flags().StringVar(&databaseSslCert, "db-ssl-cert", "", "database ssl connection certificate path")
+	cmd.Flags().StringVar(&databaseSslKey, "db-ssl-key", "", "database ssl connection key path")
+	cmd.Flags().StringVarP(&bind, "bind", "b", "0.0.0.0", "address to bind to")
+	cmd.Flags().Uint16VarP(&port, "port", "p", 8080, "port to listen on")
+	cmd.Flags().BoolVar(&profile, "profile", false, "register net/http/pprof handlers")
+	cmd.Flags().StringVar(&tlsCert, "tls-cert", "", "path to TLS certificate")
+	cmd.Flags().StringVar(&tlsKey, "tls-key", "", "path to TLS keyfile")
+	cmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "display additional output")
+	cmd.Flags().BoolVarP(&version, "version", "V", false, "display version and exit")
+	cmd.Flags().SetInterspersed(true)
+
+	cmd.CompletionOptions.HiddenDefaultCmd = true
+
+	cmd.Flags().SetInterspersed(true)
+
+	cmd.SetHelpCommand(&cobra.Command{
+		Hidden: true,
+	})
+
+	cmd.SetVersionTemplate("commands v{{.Version}}\n")
+
+	cmd.SilenceErrors = true
+
+	cmd.Version = ReleaseVersion
+
+	log.SetFlags(0)
 
 	err := cmd.Execute()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initializeConfig(cmd *cobra.Command) {
+	v := viper.New()
+
+	v.SetEnvPrefix("commands")
+
+	v.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+
+	v.AutomaticEnv()
+
+	bindFlags(cmd, v)
+}
+
+func bindFlags(cmd *cobra.Command, v *viper.Viper) {
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		configName := strings.ReplaceAll(f.Name, "-", "_")
+
+		if !f.Changed && v.IsSet(configName) {
+			val := v.Get(configName)
+			cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
+		}
+	})
 }
---
+++ web.go
@@ -0,0 +1,331 @@
+/*
+Copyright © 2025 Seednode <seednode@seedno.de>
+*/
+
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"text/template"
+	"time"
+
+	"net/http/pprof"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+const (
+	logDate string = `2006-01-02T15:04:05.000-07:00`
+)
+
+var templateFuncs = template.FuncMap{"rangeStruct": RangeStructer}
+
+var htmlTemplate = `{{range .}}        <tr>
+{{range rangeStruct .}}          <td>{{.}}</td>
+{{end}}        </tr>
+{{end}}`
+
+func RangeStructer(args ...interface{}) []interface{} {
+	if len(args) == 0 {
+		return nil
+	}
+
+	v := reflect.ValueOf(args[0])
+	if v.Kind() != reflect.Struct {
+		return nil
+	}
+
+	out := make([]interface{}, v.NumField())
+	for i := range v.NumField() {
+		out[i] = v.Field(i).Interface()
+	}
+
+	return out
+}
+
+func securityHeaders(w http.ResponseWriter) {
+	w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
+	w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
+	w.Header().Set("Cross-Origin-Resource-Policy", "same-site")
+	w.Header().Set("Permissions-Policy", "geolocation=(), midi=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), fullscreen=(), payment=()")
+	w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+	w.Header().Set("X-Xss-Protection", "1; mode=block")
+}
+
+func GenerateHeader(commandCount, totalCommandCount, failedCommandCount int) string {
+	htmlHeader := `<html>
+  <style>
+    table {
+      border: 2px solid #aaa;
+      table-layout: fixed;
+    }
+    tr:nth-child(even) {
+      background: #f4f4f4;
+    }
+    th,td {
+      padding: 0.1em 0.5em;
+    }
+    td {
+      border: 1px solid #aaa;
+    }
+    th {
+      background: #eee;
+      border: 1px solid #aaa;
+      font-weight: bold;
+      text-align: center;
+    }
+  </style>
+  <head>
+    <title>Command History</title>
+  </head>
+  <body>
+  `
+
+	htmlHeader += fmt.Sprintf("  <h3>Displaying up to %v out of %v commands, including %v non-zero exit codes.</h3>",
+		strconv.Itoa(commandCount),
+		strconv.Itoa(totalCommandCount),
+		strconv.Itoa(failedCommandCount))
+
+	htmlHeader += `
+    <table>
+      <thead>
+        <tr>
+          <th>row</th><th>start_time</th><th>duration</th><th>host_name</th><th>command_name</th><th>exit_code</th>
+        </tr>
+      </thead>
+      <tbody>
+`
+
+	return htmlHeader
+}
+
+func GenerateFooter() string {
+	htmlFooter := `      </tbody>
+    </table>
+  </body>
+</html>`
+
+	return htmlFooter
+}
+
+func ConstructPage(w io.Writer, database *Database, parameters *Parameters) error {
+	startTime := time.Now()
+
+	results, totalCommandCount, failedCommandCount, err := RunQuery(database, parameters)
+	if err != nil {
+		return err
+	}
+
+	t := template.New("t").Funcs(templateFuncs)
+	t, err = t.Parse(htmlTemplate)
+	if err != nil {
+		return err
+	}
+
+	htmlHeader := GenerateHeader(parameters.CommandCount, totalCommandCount, failedCommandCount)
+	_, err = io.WriteString(w, htmlHeader)
+	if err != nil {
+		return err
+	}
+
+	err = t.Execute(w, results)
+	if err != nil {
+		return err
+	}
+
+	htmlFooter := GenerateFooter()
+	_, err = io.WriteString(w, htmlFooter)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Constructed HTML page for up to %v commands (%v total, %v failed) in %v.\n",
+		parameters.CommandCount,
+		totalCommandCount,
+		failedCommandCount,
+		time.Since(startTime))
+
+	return nil
+}
+
+func ServePageHandler(database *Database) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		var commandCount int
+		commandCount, err := strconv.Atoi(r.URL.Query().Get("count"))
+		if err != nil {
+			commandCount = 1000
+		}
+
+		var exitCode int
+		exitCode, err = strconv.Atoi(r.URL.Query().Get("exit_code"))
+		if err != nil {
+			exitCode = -1
+		}
+
+		hostName := r.URL.Query().Get("host_name")
+
+		commandName := r.URL.Query().Get("command_name")
+
+		sortBy := r.URL.Query().Get("sort_by")
+		switch sortBy {
+		case "duration":
+			sortBy = "duration"
+		case "host_name":
+			sortBy = "hostname"
+		case "command_name":
+			sortBy = "commandname"
+		case "exit_code":
+			sortBy = "exitcode"
+		default:
+			sortBy = "starttime"
+		}
+
+		sortOrder := r.URL.Query().Get("sort_order")
+		if sortOrder != "asc" {
+			sortOrder = "desc"
+		}
+
+		parameters := &Parameters{
+			CommandCount: commandCount,
+			ExitCode:     exitCode,
+			HostName:     hostName,
+			CommandName:  commandName,
+			SortBy:       sortBy,
+			SortOrder:    sortOrder,
+		}
+
+		w.Header().Add("Content-Type", "text/html")
+
+		securityHeaders(w)
+
+		err = ConstructPage(w, database, parameters)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func ServerError(w http.ResponseWriter, r *http.Request, i interface{}) {
+	w.WriteHeader(http.StatusInternalServerError)
+
+	w.Header().Add("Content-Type", "text/plain")
+
+	securityHeaders(w)
+
+	w.Write([]byte("500 Internal Server Error\n"))
+}
+
+func ServerErrorHandler() func(http.ResponseWriter, *http.Request, interface{}) {
+	return ServerError
+}
+
+func ServeVersion() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		data := []byte(fmt.Sprintf("commands v%s\n", ReleaseVersion))
+
+		securityHeaders(w)
+
+		w.Header().Write(bytes.NewBufferString("Content-Length: " + strconv.Itoa(len(data))))
+
+		w.Write(data)
+	}
+}
+
+func ServePage() error {
+	var err error
+
+	timeZone := os.Getenv("TZ")
+	if timeZone != "" {
+		time.Local, err = time.LoadLocation(timeZone)
+		if err != nil {
+			return err
+		}
+	}
+
+	if verbose {
+		fmt.Printf("%s | START: commands v%s\n",
+			time.Now().Format(logDate),
+			ReleaseVersion,
+		)
+	}
+
+	bindHost, err := net.LookupHost(bind)
+	if err != nil {
+		return err
+	}
+
+	bindAddr := net.ParseIP(bindHost[0])
+	if bindAddr == nil {
+		return errors.New("invalid bind address provided")
+	}
+
+	if databaseType != "cockroachdb" && databaseType != "postgresql" {
+		return errors.New("invalid database type specified")
+	}
+
+	databaseURL, err := GetDatabaseURL()
+	if err != nil {
+		return err
+	}
+
+	database := &Database{
+		Url:   databaseURL,
+		Table: databaseTable,
+	}
+
+	mux := httprouter.New()
+
+	mux.PanicHandler = ServerErrorHandler()
+
+	mux.GET("/", ServePageHandler(database))
+
+	mux.GET("/version", ServeVersion())
+
+	if profile {
+		mux.HandlerFunc("GET", "/debug/pprof/", pprof.Index)
+		mux.HandlerFunc("GET", "/debug/pprof/cmdline", pprof.Cmdline)
+		mux.HandlerFunc("GET", "/debug/pprof/profile", pprof.Profile)
+		mux.HandlerFunc("GET", "/debug/pprof/symbol", pprof.Symbol)
+		mux.HandlerFunc("GET", "/debug/pprof/trace", pprof.Trace)
+	}
+
+	srv := &http.Server{
+		Addr:         net.JoinHostPort(bind, strconv.Itoa(int(port))),
+		Handler:      mux,
+		IdleTimeout:  10 * time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Minute,
+	}
+
+	if tlsKey != "" && tlsCert != "" {
+		fmt.Printf("%s | Listening on %s://%s/\n",
+			time.Now().Format(logDate),
+			scheme,
+			srv.Addr)
+
+		err = srv.ListenAndServeTLS(tlsCert, tlsKey)
+	} else {
+		fmt.Printf("%s | Listening on %s://%s/\n",
+			time.Now().Format(logDate),
+			scheme,
+			srv.Addr)
+
+		err = srv.ListenAndServe()
+	}
+
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return nil
+}
